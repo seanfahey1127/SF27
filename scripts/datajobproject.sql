ef596cda1453d28f06779d3f607a55264385bdab
seanfahey1127 <seanfahey1127@gmail.com>
2025-02-06 20:23:22 -0500

    Done

--- scripts/datajobproject.sql
+++ scripts/datajobproject.sql
@@ -108,7 +108,7 @@ WHERE title LIKE '%Analyst%';
 
 /* 12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
-Answer: 26, they all have spelling errors  */ 
+Answer: 26, Data */ 
 
 
 SELECT COUNT (DISTINCT title)
@@ -118,5 +118,32 @@ WHERE title NOT LIKE '%Analyst%' AND title NOT LIKE '%Analytics%';
 SELECT DISTINCT title 
 FROM data_analyst_jobs
 WHERE title NOT LIKE '%Analyst%' AND title NOT LIKE '%Analytics%';
+
+
+SELECT DISTINCT title 
+FROM data_analyst_jobs
+WHERE title LIKE '%ANALYST%' AND title LIKE '%ANALYTICS';
+
+
+/* **BONUS:**
+You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+ - Disregard any postings where the domain is NULL. 
+ - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+  - Which three industries are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top 3?
+  
+  			Answers: 25 industries 
+  			Top 3 industries are: 
+  			Internet and Software with 52 jobs
+			Banks and Financial Services with 50 jobs
+			Health Care with 35 jobs       		*/
+
+SELECT domain, COUNT(*) AS jobs
+FROM data_analyst_jobs
+WHERE skill LIKE '%SQL%'
+AND days_since_posting <= '21'
+AND domain IS NOT NULL 
+GROUP BY domain
+ORDER BY jobs DESC;
+
 
 
